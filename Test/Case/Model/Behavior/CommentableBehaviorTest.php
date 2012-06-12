d44ae7b09bf38fb11d8bf3a3e7997f2064811c5d
dr. Hannibal Lecter <lecterror@gmail.com>
2012-06-12 15:22:57 +0200

    added tests for commentable behaviour

---
+++ Test/Case/Model/Behavior/CommentableBehaviorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+App::uses('CommentableBehavior', 'Feedback.Model/Behavior');
+
+/**
+ * CommentableBehavior Test Case
+ *
+ * @property Model $Article
+ */
+class CommentableBehaviorTest extends CakeTestCase
+{
+	public $fixtures = array
+		(
+			'plugin.feedback.article',
+			'plugin.feedback.comment',
+		);
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->Article = ClassRegistry::init('Article');
+	}
+
+	public function tearDown()
+	{
+		unset($this->Article);
+
+		parent::tearDown();
+	}
+
+	public function testAssociations()
+	{
+		$this->assertArrayNotHasKey('Comment', $this->Article->hasMany);
+
+		$this->Article->Behaviors->attach('Feedback.Commentable');
+		$this->assertArrayHasKey('Comment', $this->Article->hasMany);
+
+		$expected = array
+			(
+				'className'		=> 'Feedback.Comment',
+				'conditions'	=> 'Comment.foreign_model = \'Article\'',
+				'foreignKey'	=> 'foreign_id',
+			);
+
+		foreach ($expected as $key => $value)
+		{
+			$this->assertArrayHasKey($key, $this->Article->hasMany['Comment']);
+			$this->assertEquals($expected[$key], $this->Article->hasMany['Comment'][$key]);
+		}
+
+		$this->Article->Behaviors->detach('Commentable');
+		$this->assertArrayNotHasKey('Comment', $this->Article->hasMany);
+	}
+
+	public function testFind()
+	{
+		$this->Article->Behaviors->attach('Feedback.Commentable');
+
+		$result = $this->Article->find('first', array('conditions' => array('id' => 2)));
+		$expected = array
+			(
+				'Article' => array
+				(
+					'id' => '2',
+					'title' => 'This is my other title',
+					'content' => 'But this one has no references in it. Sorry about that old chap.'
+				),
+				'Comment' => array
+				(
+					array
+					(
+						'id' => '3',
+						'foreign_model' => 'Article',
+						'foreign_id' => '2',
+						'user_id' => null,
+						'author_ip' => '127.0.0.1',
+						'author_name' => 'newjm',
+						'author_email' => 'nejmich@example.com',
+						'author_website' => 'www.example.com',
+						'content' => 'zis iz ze komment..',
+						'created' => '2012-04-18 22:10:54'
+					)
+				)
+			);
+
+		$this->assertEquals($expected, $result);
+	}
+}
